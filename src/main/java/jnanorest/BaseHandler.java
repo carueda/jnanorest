bc92b8908ef7aebfcbc129702213824bda36c926
Carlos Rueda <carueda@mbari.org>
2014-01-24 20:36:27 -0800

    report any exception as an internal server error, including stack trace

--- src/main/java/jnanorest/BaseHandler.java
+++ src/main/java/jnanorest/BaseHandler.java
@@ -7,8 +7,10 @@ import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URI;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -25,9 +27,35 @@ class BaseHandler implements HttpHandler {
     }
 
     public void handle(HttpExchange exchange) throws IOException {
+        try {
+            Req req = createReq(exchange);
+            Res res = createRes(exchange);
+            _handle(exchange, req, res);
+        }
+        catch(Throwable t) {
+            // respond with "internal server error"
+            Headers resHeaders = exchange.getResponseHeaders();
+            Map<String, Object> map2 = new HashMap<String, Object>();
+            map2.put("class", t.getClass().getName());
+            map2.put("message", t.getMessage());
+            map2.put("stacktrace", t.getStackTrace());
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("error", map2);
 
-        Req req = createReq(exchange);
-        Res res = createRes(exchange);
+            String body = new Json().stringify(map);
+            byte[] bytes = body.getBytes();
+            resHeaders.set("Content-Type", "application/json");
+            int status = 500;
+            exchange.sendResponseHeaders(status, bytes.length);
+            OutputStream responseBody = exchange.getResponseBody();
+            responseBody.write(bytes);
+            responseBody.close();
+
+            clf(exchange, exchange.getRequestURI(), status, bytes.length);
+        }
+    }
+
+    private void _handle(HttpExchange exchange, Req req, Res res) throws IOException {
 
         String requestMethod = exchange.getRequestMethod();
         if (requestMethod.equalsIgnoreCase("GET")) {
@@ -68,7 +96,7 @@ class BaseHandler implements HttpHandler {
         responseBody.write(bytes);
         responseBody.close();
 
-        clf(exchange, req, res, bytes.length);
+        clf(exchange, req.uri, res.status, bytes.length);
     }
 
     private Req createReq(HttpExchange exchange) throws IOException {
@@ -92,14 +120,14 @@ class BaseHandler implements HttpHandler {
     private static SimpleDateFormat df = new SimpleDateFormat("dd/MMM/yyyy:hh:mm:ss Z");
 
     // prints a line a la http://httpd.apache.org/docs/1.3/logs.html#common
-    private static void clf(HttpExchange exchange, Req req, Res res, int bodyLen) {
+    private static void clf(HttpExchange exchange, URI uri, int status, int bodyLen) {
         String h = exchange.getRemoteAddress().getHostName();
         String l = "-";  // rfc1413
         String u = "-";
         String t = df.format(new Date());
         String r = String.format("%s %s %s",
-                exchange.getRequestMethod(), req.uri, exchange.getProtocol());
-        String s = String.valueOf(res.status);
+                exchange.getRequestMethod(), uri, exchange.getProtocol());
+        String s = String.valueOf(status);
         String b = String.valueOf(bodyLen);
 
         System.out.printf("%s %s %s [%s] \"%s\" %s %s%n",
